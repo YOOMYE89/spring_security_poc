b7fc570e3f4858bc4c42e972fb0f7d45de024a66
MH YOO <mhyoo@MHui-MacBookAir.local>
2023-03-27 22:30:49 +0900

    custom

--- src/main/java/app/config/security/SecurityConfig.java
+++ src/main/java/app/config/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package app.config.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -32,14 +35,18 @@ public class SecurityConfig {
 
                 // 인증 방식
                 .formLogin()
-
-
         ;
         return server.build();
     }
 
 
-
+    @Bean
+    public WebSecurityCustomizer webSecurity() {
+        return web -> web
+                .ignoring()
+                // 기본 정적 자원에 대한 ignore
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -57,14 +64,16 @@ public class SecurityConfig {
         manager.createUser(
                 User.withUsername("manager")
                         .password(password)
-                        .roles("MANAGER")
+                        // TODO 계층구조 설정
+                        .roles("USER","MANAGER")
                         .build()
         );
 
         manager.createUser(
                 User.withUsername("admin")
                         .password(password)
-                        .roles("ADMIN")
+                        // TODO 계층구조 설정
+                        .roles("USER","MANAGER","ADMIN")
                         .build()
         );
 
